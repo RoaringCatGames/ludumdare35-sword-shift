9283a32e475124b6d6c4c63eefa583c7d5573f88
Rex Soriano <awesomerex@gmail.com>
2016-04-18 12:28:02 -0400

    messing with default values

--- core/src/com/roaringcatgames/ludumdare/thirtyfive/systems/PlayerSystem.java
+++ core/src/com/roaringcatgames/ludumdare/thirtyfive/systems/PlayerSystem.java
@@ -93,7 +93,7 @@ public class PlayerSystem extends IteratingSystem implements InputProcessor {
             player.add(BoundsComponent.create(engine)
                     .setBounds(3f, 3f, 1f, 1f));
             player.add(StateComponent.create(engine)
-                    .set("HATCHET_IDLE")
+                    .set("HAMMER_IDLE")
                     .setLooping(true));
             player.add(TextureComponent.create(engine));
             player.add(AnimationComponent.create(engine)
@@ -218,7 +218,7 @@ public class PlayerSystem extends IteratingSystem implements InputProcessor {
                 case HAMMER:
                     player.add(RotateToComponent.create(getEngine())
                             .addRotateTo(90f, 600f)
-                            .addRotateTo(-120f, -900f)
+                            .addRotateTo(-200f, -900f)
                             .addRotateTo(0f, 3000f));
                     break;
                 case KATANA:
